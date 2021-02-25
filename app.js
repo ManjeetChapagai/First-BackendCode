17ac4fab67b089c9c551d9851b7a3df16a87102b
ManjeetChapagai <74332423+ManjeetChapagai@users.noreply.github.com>
2021-02-25 03:19:03 -0500

    first-commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+require("dotenv/config");
+
+app.use(bodyParser.json());
+//Import route
+const postsRoute = require("./routes/posts");
+
+//Middlewares
+app.use("/posts", postsRoute);
+
+app.get("/", (req, res) => {
+  res.send("We are at home");
+});
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, () =>
+  console.log("connected to DB")
+);
+//How to listen to the server
+app.listen(3000);
